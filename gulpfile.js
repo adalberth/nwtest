6854febeb7a7937dcd7a94d68f8dc2ad7c6bd2a1
David Adalberth Andersen <david@stupid-studio.com>
2015-01-29 12:48:56 +0100

    Added uglify to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var	livereload = require('gulp-livereload');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
-var browserify = require('browserify')
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
 
 
 var bundler = watchify(browserify('./src/js/app.js'));
@@ -13,11 +14,15 @@ var bundler = watchify(browserify('./src/js/app.js'));
 
 // Bundle
 function bundle(){
+	//https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+	//https://github.com/gulpjs/gulp/tree/master/docs/recipes
+
 	return bundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({loadMaps: true}))
+			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist'))
     .pipe(livereload());
