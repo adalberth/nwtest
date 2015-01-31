97d47bfefd0febb925f835bb17324c3d9971b989
David Adalberth Andersen <david@stupid-studio.com>
2015-01-31 15:02:10 +0100

    Working on countdown

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,52 +1,115 @@
-function createCountdown(opts){
+function createCountdown(opts, callback){
 	'use strict';
 	
 	if(opts === undefined){
-		console.error('[createCountdown] Options not defined');
+		console.log('[createCountdown] Options not defined');
+		return;
+	}
+
+	if(callback === undefined){
+		console.log('[createCountdown] Callback not defined');
 		return;
 	}
 
 	var self = {};
 
-
 	var ti = null;
-	var miliseconds = 0; //100 * 60 * 4;
+	var miliseconds;
 	var lastTime = performance.now();
 	var delay = 10;
+	var startTime = opts.seconds * 100 || 100 * 60 * 4;
+
+	var callback = callback || function(){console.log('done')};
+	var update = opts.onUpdate || function(){};
 
 	var time = {
 		minutes:0,
 		seconds:0,
 		miliseconds:0
-	}
+	};
 
-	timeout(); 
+	init();
+
+	function init(){
+		reset();
+	}
 
 	function timeout(){
 		delay = 10 - (performance.now() - lastTime);
-		
+
 		ti = setTimeout(function(){
 		
 			lastTime = performance.now();
 			
-			time = {
-				minutes: parseInt((miliseconds / 6000) % 60),
-				seconds: parseInt((miliseconds / 100) % 60),
-				miliseconds: miliseconds % 100
-			};
+			setTimeObject(miliseconds);
 
-			printTime();
+			miliseconds -= 1; 
 
-			miliseconds += 1;  
-			timeout();
+			if(miliseconds <= 0){
+				callback();
+			}else{
+				update(getTime());
+				clearTimeout(ti); 
+				timeout();
+			}
+
 
 		}, delay);
 	}
 
+	function getFormatedTime(){
 
-	function printTime(){
-		console.log(time.minutes,time.seconds,time.miliseconds);
+		function leadingZero(value){
+			return value < 10 ? "0" + value : value;
+		}
+
+		return {
+			minutes: leadingZero(time.minutes),
+			seconds: leadingZero(time.seconds),
+			miliseconds: leadingZero(time.miliseconds)
+		}
 	}
+
+	function setTimeObject(value){
+		time = {
+			minutes: parseInt((value / 6000) % 60),
+			seconds: parseInt((value / 100) % 60),
+			miliseconds: value % 100
+		};
+	}
+
+	function setTime(val){
+		miliseconds = val;
+	}
+
+	function reset(){
+		miliseconds = startTime;
+		setTimeObject(miliseconds);
+	}
+
+	function start(){
+		timeout(); 
+	}
+
+	function stop(){
+		clearTimeout(ti); 
+	}
+
+	function getTime(){
+		return {
+			miliseconds: miliseconds,
+			process: (miliseconds / startTime) * 100,
+			time: time,
+			formatedTime: getFormatedTime()
+		};
+	}
+
+
+	self.start = start;
+	self.stop = stop;
+	self.reset = reset;
+	self.getTime = getTime;
+
 	return self;
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,58 @@
 //https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
 
 var createCountdown = require('./countdown');
-var countdown = createCountdown({});
+
+
+
+/*
+* CountDown
+*/
+var countdownBackground = document.querySelectorAll('.countdown-background')[0];
+var countdownTime = document.querySelectorAll('.countdown-time');
+
+var countdown = createCountdown({
+	seconds:60 * 4,
+	onUpdate:countdownOnUpdateHandler
+},function(){
+	console.log("Timer done");
+});
+
+
+countdownRefresh();
+
+function countdownRefresh(){
+	drawTimer(countdown.getTime());
+}
+
+function countdownOnUpdateHandler(obj){
+	// countdownBackground.style.transform = "translate3d("+ (obj.process - 100) +"%,0,0)";
+	drawTimer(obj);
+}
+
+function drawTimer(obj){
+	countdownBackground.style.width = obj.process + "%";
+	
+	for (var i = 0; i < countdownTime.length; i++) {
+		countdownTime[i].innerHTML = obj.formatedTime.minutes + " " + obj.formatedTime.seconds + " " + obj.formatedTime.miliseconds;
+	};
+}
+
+/*
+* UI
+*/
+var start = document.querySelectorAll('.ui-button-start')[0];
+var stop = document.querySelectorAll('.ui-button-stop')[0];
+var reset = document.querySelectorAll('.ui-button-reset')[0];
+
+start.addEventListener('click', function(){
+	countdown.start();
+});
+
+stop.addEventListener('click', function(){
+	countdown.stop();
+});
+
+reset.addEventListener('click', function(){
+	countdown.reset();
+	countdownRefresh();
+});
